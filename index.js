16e228c52b1441b8275191f5def84560f6c8f34f
Hardik Sharma <sharmahardikaz@gmail.com>
2023-06-29 15:58:02 +0530

    Implemented JavaScript

---
+++ index.js
@@ -0,0 +1,56 @@
+
+document.querySelectorAll("img")[0].addEventListener("click",
+ function(){
+
+var audio = new Audio('sounds/tom-1.mp3');
+audio.play();
+
+});
+
+document.querySelectorAll("img")[1].addEventListener("click",
+ function(){
+
+var audio = new Audio('sounds/tom-2.mp3');
+audio.play();
+
+});
+
+document.querySelectorAll("img")[2].addEventListener("click",
+ function(){
+
+var audio = new Audio('sounds/tom-3.mp3');
+audio.play();
+
+});
+
+document.querySelectorAll("img")[3].addEventListener("click",
+ function(){
+
+var audio = new Audio('sounds/tom-4.mp3');
+audio.play();
+
+});
+
+document.querySelectorAll("img")[4].addEventListener("click",
+ function(){
+
+var audio = new Audio('sounds/kick-bass.mp3');
+audio.play();
+
+});
+
+document.querySelectorAll("img")[5].addEventListener("click",
+ function(){
+
+var audio = new Audio('sounds/snare.mp3');
+audio.play();
+
+});
+
+document.querySelectorAll("img")[6].addEventListener("click",
+ function(){
+
+var audio = new Audio('sounds/crash.mp3');
+audio.play();
+
+});
